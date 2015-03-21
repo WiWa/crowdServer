ccd11e0b56f5e5636d3eba80417952faf8e4b445
resonantjacket5 <wesleylin625@gmail.com>
2015-03-21 13:32:23 -0500

    changing m and N now can view web pages 8000/8080 port

--- centralServer/serverM.js
+++ centralServer/serverM.js
@@ -1,13 +1,34 @@
+
+//used to interact with uploading
+var querystring = require ('querystring');
+var	fs = require('fs');
+var	formidable = require ('formidable');
+
+//to connect with sockets and see webpage
 var net = require('net');
 var http = require('http');
 
+//port number
 var HOST = '127.0.0.1';
 var PORT = 3000;
 
-body='<html><body>Hi, not connected yet!</body></html>'
-
+head	='<html>'
+body	='<body>Hi, this is the Main Server; not connected</body>'
+end		='</html>'
 http.createServer( function(request, response) {
 	
+	var button = '<head>'+
+	'<meta http-equiv="Content-Type" content="text/html; '+
+	'charset=UTF-8" />'+
+	'</head>'+'<form action="/upload" enctype = "multipart/form-data" '+
+	'method="post">'+
+	'<input type="file" name = "upload">'+
+	'<input type="submit" value = "Upload file"/>' +
+	'</form>';
+	/*'<form action="/upload" method="post">'+
+	'<textarea name="text" rows="20" cols="60"></textarea>'+
+	'<input type="submit" value="Submit text" />'*/
+	displayed=head+button+body+end;
 	response.writeHead(200,{"Content-Type":"text/html"});
 	response.write(body);
 	response.end();
--- centralServer/serverN.js
+++ centralServer/serverN.js
@@ -1,15 +1,35 @@
 
 var net = require('net');
 var fs = require('fs');
+var http = require('http');
 
 var HOST ='127.0.0.1';
 var PORT = 3000;
+//port of the sockets
+
+
+body='<html><body>Hi this is the little server.'
++' Still waiting for page from central Server </body></html>'
+
+http.createServer( function(request, response) {
+	
+	response.writeHead(200,{"Content-Type":"text/html"});
+	//need to go 
+	response.write(body);
+	response.end();
+}).listen(8000);
+//where you go to see the page
+/**/
+
+
 
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
 
 	console.log('CONNECTED TO: ' + HOST + ':' + PORT);
 	client.write('I am Chuck Norris!');
+
+	//client.read(something)
 	//client.write(/index.html);
 });
 
