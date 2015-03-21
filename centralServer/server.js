1b5332c1300481a2803adf35d27b281cfa4bb72f
Eric <eric.l.yang@vanderbilt.edu>
2015-03-21 11:34:09 -0500

    first commit

--- centralServer/server.js
+++ centralServer/server.js
@@ -5,10 +5,14 @@ function start()
 {
 	function onRequest(request, response)
 	{
+		/*response.writeHead(301,{Location: 'http://127.0.0.1:3000'}
+		);
+		response.end();*/
+		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("SSN and Credit Card #: ");
 		response.end();
 	}
-	http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(8888, '127.0.0.1');
 	console.log("Server has started.");
 }
 
