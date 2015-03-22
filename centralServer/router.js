f702e2bb35bd02a5e0174d1a4e711dafeb7f71f5
Win Wang <fallenpwr@gmail.com>
2015-03-21 21:43:23 -0400

    app routing barebones

--- centralServer/router.js
+++ centralServer/router.js
@@ -1,6 +1,19 @@
 var app = require('./server.js')
 
 app.get('/',function(req,res){
-      res.sendfile("index.html");
+  res.sendfile("index.html");
 });
 
+app.get('/:app_name', function(req, res){
+  //app.redirect('/app/'+Applications.findOne({name: req.params.app_name})['id']')
+})
+
+app.get('/app/:id', function(req, res){
+  findClientForAppID(req.params.id)
+})
+
+function findClientForAppID(id){
+
+  var client = ""
+  //app.redirect(client['host']+':'+client['port'])
+}
