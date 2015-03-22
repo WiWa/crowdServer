f4d4afd6d31e6488eeb2e91aeb420197f1f413a9
amynguyen780 <anguyen2229@gmail.com>
2015-03-21 20:53:40 -0500

    integrate database logic into server

--- database.js
+++ database.js
@@ -1,61 +1,26 @@
-var express=require("express");
-var multer  = require('multer');
-var app = module.exports = express();
-var done=false;
-
 var mongoose = require(‘mongoose’);
 mongoose.connect(‘mongo db://localhost/test’); //pending connection to test database
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
 }); //test if connection is successful
-
-/*Configure the multer.*/
-
-app.use(multer({ dest: './uploads/',
- rename: function (fieldname, filename) {
-    return filename+Date.now();
-  },
-onFileUploadStart: function (file) {
-  console.log(file.originalname + ' is starting ...')
-},
-onFileUploadComplete: function (file) {
-  console.log(file.fieldname + ' uploaded to  ' + file.path)
-  done=true;
-}
-}));
-
-/*Handling routes.*/
-require('./router.js');
-
-/*Run the server.*/
-app.listen(3000,function(){
-    console.log("Working on port 3000");
-});
-
 var clientSchema = mongoose.Schema({
 	host: String,
 	port: String,
 	busy: Boolean
 }; //reference to a Schema and definition of client
 var Client = mongoose.model(‘Client’, clientSchema); //compile Schema into a model
-function makeClient(host, port) {
-	var host = “”; //TBD
-	var port = “”; //TBD
-	var client1 = new Client({host: host, port: port, busy: false});
-	client1.save(function(err, client1) {
-		if(err) return console.error(err);
-	}); //save to database
-}
-
-
-
+var host = “”; //TBD
+var port = “”; //TBD
+var client1 = new Client({host: host, port: port, busy: false});
+client1.save(function(err, client1) {
+	if(err) return console.error(err);
+}); //save to database
 var appSchema = mongoose.Schema({
 	name: String,
 	clientele: Array
 }); //Reference to a Schema and definition of app
 var App = mongoose.model(‘App, appSchema); //compile Schema into a model
-function makeApp() {
 var name = “”; //TBD
 var clientele = []; //will represent array of clients that have been delegated the app	
 var appFound = App.findOne({name: name}, function(err, name of app variable) {}); 
@@ -67,4 +32,4 @@ if(appFound == null) {
 }); 
 }
 clientele[0] = Client.findOne({busy: false}, function(err, clients) {}); //find all clients that are not busy
-}
+
